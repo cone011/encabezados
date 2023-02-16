c210f973b45c7ddcab456a5b9f599d0d91bf9167
Juan Caimen <jjcaimen@gmail.com>
2023-02-16 16:31:27 -0300

    commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+
 app.set("view engine", "pug");
-app.search("views", "views");
+app.set("views", "views");
 
 app.get("/", (req, res) => {
-  res.send(req.header["user-agent"]);
+  res.send(req.headers["user-agent"]);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
